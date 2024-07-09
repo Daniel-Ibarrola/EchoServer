db032c6d0854ac8ebdf3fce5ecda6b23efad2733
Daniel Ibarrola <dis96051@gmail.com>
2024-07-08 19:13:54 -0600

    Set up project structure

---
+++ src/echo/main.py
@@ -0,0 +1,8 @@
+
+
+def main():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    print(main())
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from echo.main import main
+
+
+def test_main():
+    assert main() == "Hello World"
